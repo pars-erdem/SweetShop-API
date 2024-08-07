223d8556361817ef086496a6f4ab59f92af74751
Erdem Mansurgüler <erdemmansurguler1@gmail.com>
2024-08-07 09:54:27 +0300

    chore: migrate database to PostgreSQL and implement security measures

--- products/api/permissions.py
+++ products/api/permissions.py
@@ -1,7 +1,19 @@
 from rest_framework import permissions
-
-
-class IsAdminOrReadOnly(permissions.IsAdminUser):
+class IsSuperUserOrReadWrite(permissions.BasePermission):
     def has_permission(self, request, view):
-        is_admin = super().has_permission(request, view)
-        return request.method in permissions.SAFE_METHODS or is_admin
+        if request.user.is_superuser:
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.method in ['PUT', 'PATCH']:
+            return request.user and request.user.is_authenticated
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.method in ['PUT', 'PATCH']:
+            return request.user and request.user.is_authenticated
+        return False
